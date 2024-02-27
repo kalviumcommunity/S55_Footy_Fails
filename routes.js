f169a2340e5893eafb285780748ea3f404dd1f47
divyamprabhudessai <divyam.desai@kalvium.community>
2024-02-27 11:54:49 +0530

    added crud operations in routes.js and their bruno testfiles

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const {getConnectionStatus}=require('./db')
 
+router.use(express.json());
 
 router.get('/',async  (req, res) => {
     const finalStatus = await getConnectionStatus()
@@ -12,5 +13,24 @@ router.get("/ping", (req, res) => {
     res.send('pong');
 
 });
+
+router.post("/post",(req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
+})
+
+router.put('/put',async(req,res)=>{
+    let finalStatus = await getConnectionStatus()
+    finalStatus = "hey yall"
+    res.send(finalStatus)
+})
+
+
+router.delete('/delete', async (req, res) => {
+    
+    res.send('Data deleted successfully');
+});
+
+
 
 module.exports = router
